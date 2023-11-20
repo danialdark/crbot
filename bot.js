d0652f2288756dee771afc441fe4203e365a8b1d
Danial <danialdark12098@gmail.com>
2023-11-20 14:46:15 +0330

    debug

--- bot.js
+++ bot.js
@@ -19,8 +19,8 @@ const client2 = new Redis({
 });
 
 
-async function makeMessage(data) {
-  const changedData = JSON.parse(key, data, server)
+async function makeMessage(key, data, server) {
+  const changedData = JSON.parse(data)
   const formattedDateTime = moment(changedData["1m"][0].t).utcOffset(0).format('YYYY-MM-DD HH:mm:ss');
 
   return `${key}: ${changedData["1m"][0].c} time: ${formattedDateTime} on ${server} Server`

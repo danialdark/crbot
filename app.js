ee8b41266f686efe8d4687466e7d7185a631fc0b
Danial <danialdark12098@gmail.com>
2023-11-20 14:10:20 +0330

    test finished

--- app.js
+++ app.js
@@ -1,69 +1,56 @@
 const TelegramBot = require('node-telegram-bot-api');
-// Replace 'YOUR_BOT_TOKEN' with the actual token you obtained from the BotFather
 const bot = new TelegramBot('6372105023:AAENq18ynY1CIpQI7QCUZRkT9UD3aL2AWNE', { polling: true });
-
-// Replace '@your_channel_username' with your channel username
 const channelUsername = '@crtrest';
-const redis = require('redis');
 
-// Create connections to the three Redis instances
-const nvmeGerman = redis.createClient({ host: '157.90.39.38', port: 6379, password: 'D@n!@l12098', enableCompression: true });
-const sataGerman = redis.createClient({ host: '31.40.4.226', port: 6379, password: 'D@n!@l12098', enableCompression: true });
+const Redis = require('ioredis');
+
+// Connect to the first Redis server
+const client1 = new Redis({
+  host: '95.217.151.65',
+  port: 6379,
+  password: "D@n!@l12098",
+});
 
 
-// Example keys to retrieve from each Redis instance
-const nvmeGermanKey = 'btcusdt';
-const sataGermanKey = 'btcusdt';
+const client2 = new Redis({
+  host: '157.90.39.38',
+  port: 6379,
+  password: "D@n!@l12098",
+});
 
 
-// Function to get data from Redis and log the result
-function getDataFromRedis(client, key) {
-  return new Promise((resolve, reject) => {
-    client.get(key, (error, result) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result);
-      }
-    });
-  });
+
+async function sendMessage() {
+  // Send a message to the channel
+  await bot.sendMessage(channelUsername, message)
+
 }
 
-
-async function fetchData() {
-  // Create Redis clients for each server
-  const client1 = redis.createClient(nvmeGerman);
-  const client2 = redis.createClient(sataGerman);
-
-  try {
-    const data1 = await getDataFromRedis(client1, nvmeGermanKey);
-    await sendMessageAsync("nvme is live")
-
-    const data2 = await getDataFromRedis(client2, sataGermanKey);
-    await sendMessageAsync("sata is live")
-
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  } finally {
-    // Quit the Redis connections
-    client1.quit();
-    client2.quit();
-
+async function getRedisData(key, client, serverName) {
+  const data = await client.get(key)
+  if (data != null) {
+    sendMessage(channelUsername, `${serverName} is active`)
+  } else {
+    sendMessage(channelUsername, `***********${serverName} is deactive*******`)
   }
 }
+
 
 
 setInterval(() => {
-  fetchData()
-}, 5000);
+  getRedisData("btcusdt", client1, "nvmeData")
+  getRedisData("btcusdt", client2, "sataData")
+}, 60000);
 
 
-async function sendMessageAsync(myMessage) {
-  try {
-    // Send a message to the channel
-    const message = await bot.sendMessage(channelUsername, myMessage);
-    // console.log('Message sent:', message);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+
+
+
+
+
+
+
+
+
+
+

306d8932192fb206b220c74db2e1c04f332e0681
Danial <danialdark12098@gmail.com>
2023-12-28 10:34:47 +0330

    activating

--- bot.js
+++ bot.js
@@ -65,14 +65,13 @@ async function getRedisData(key, client, serverName) {
   }
 }
 
-getRedisData("status", client3, "Bourse")
 
 
-// setInterval(async () => {
-//   await getRedisData("btcusdt", client1, "NVME")
-//   await getRedisData("btcusdt", client2, "SATA")
-//   await getRedisData("status", client3, "Bourse")
-// }, 60000);
+setInterval(async () => {
+  await getRedisData("btcusdt", client1, "NVME")
+  await getRedisData("btcusdt", client2, "SATA")
+  await getRedisData("status", client3, "Bourse")
+}, 60000);
 
 
 
